045fd25525194a9849cf1f0ca1dafd04a30c59e3
Maha-1998 <110592938+Maha-1998@users.noreply.github.com>
2022-08-04 12:49:36 +0530

    Add files via upload

---
+++ Ec2.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" " winodws" {
+  ami           =  "ami-05912b6333beaa478"
+  instance_type = "t2.micro"
+ 
+  tags = {
+    Name = "Windows server"
+  }
+}
